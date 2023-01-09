e6c48c42bac472a3ecf4ceb31f98b4d6efdbe760
wotherspoonm <69063994+wotherspoonm@users.noreply.github.com>
2023-01-09 17:49:26 +1100

    added player invincibility

--- dont_know_yet_2.0/Assets/Scripts/Player.cs
+++ dont_know_yet_2.0/Assets/Scripts/Player.cs
@@ -9,9 +9,13 @@ public class Player : LivingEntity
     public float force = 500f;
     public LayerMask enemyMask;
     public LayerMask coinMask;
+    public float flashTime = 0.5f;
+    public int numberOfFlashes = 3;
 
-    private Vector3 moveInput;
-    private Vector3 moveDirection;
+    Vector3 moveInput;
+    Vector3 moveDirection;
+    bool isInvincible;
+
 
     public event System.Action OnCollectCoin;
 
@@ -29,7 +33,8 @@ public class Player : LivingEntity
     void OnCollisionEnter(Collision collision) {
         int collisionLayer = collision.gameObject.layer;
         int collisionLayerMask = 1 << collisionLayer;
-        if ((enemyMask.value & collisionLayerMask) != 0) {
+        if ((enemyMask.value & collisionLayerMask) != 0 && !isInvincible) {
+            StartCoroutine(AnimateInvincibility());
             base.TakeDamage(collision.gameObject.GetComponent<Enemy>().damage);
         }
     }
@@ -41,5 +46,25 @@ public class Player : LivingEntity
             OnCollectCoin();
             Destroy(other.gameObject);
         }
+    }
+
+    IEnumerator AnimateInvincibility() {
+        isInvincible = true;
+        Color originalColor = Color.green;
+        Color targetColor = Color.blue;
+
+        Material mat = GetComponent<Renderer>().material;
+
+        for (int i = 0; i < numberOfFlashes; i++) {
+            float flashSpeed = 1 / flashTime;
+            float percent = 0;
+            while (percent < 1) {
+                percent += Time.deltaTime * flashSpeed;
+                float interpolation = 1 - Mathf.Abs(2 * percent - 1);
+                mat.color = Color.Lerp(originalColor, targetColor, interpolation);
+                yield return null;
+            }
+        }
+        isInvincible = false;
     }
 }
