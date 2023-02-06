58751d11cdd76612f6f4d35702f2c5793aee60c3
wotherspoonm <69063994+wotherspoonm@users.noreply.github.com>
2023-02-06 17:49:14 +1100

    Lecture 17 - Kitchen Parent Object

--- kitchen_chaos_code_monkey/Assets/Scripts/KitchenObject.cs
+++ kitchen_chaos_code_monkey/Assets/Scripts/KitchenObject.cs
@@ -6,7 +6,27 @@ public class KitchenObject : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
+    private ClearCounter clearCounter;
+
     public KitchenObjectSO GetKitchenObjectSO() {
         return kitchenObjectSO;
+    }
+
+    public void SetClearCounter(ClearCounter clearCounter) {
+        if (this.clearCounter != null) {
+            this.clearCounter.ClearKitchenObject();
+        }
+
+        if (clearCounter.HasKitchenObject()) {
+            Debug.LogError("Counter already has a KitchenObject!");
+        }
+        clearCounter.SetKitchenObject(this);
+
+        transform.parent = clearCounter.GetKitchenObjectFollowTransform();
+        transform.localPosition = Vector3.zero;
+    }
+
+    public ClearCounter GetClearCounter() {
+        return clearCounter;
     }
 }
