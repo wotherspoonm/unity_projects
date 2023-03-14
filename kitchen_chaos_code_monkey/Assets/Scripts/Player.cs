5552f63403455e04d7fc44ef18e107daa5762191
wotherspoonm <69063994+wotherspoonm@users.noreply.github.com>
2023-03-14 22:55:51 +1100

    Lecture 36 - Game Start

--- kitchen_chaos_code_monkey/Assets/Scripts/Player.cs
+++ kitchen_chaos_code_monkey/Assets/Scripts/Player.cs
@@ -37,12 +37,16 @@ public class Player : MonoBehaviour, IKitchenObjectParent
     }
 
     private void GameInput_OnInteractAlternateAction(object sender, EventArgs e) {
+        if (!KitchenGameManager.Instance.IsGamePlaying()) return;
+
         if (selectedCounter != null) {
             selectedCounter.InteractAlternate(this);
         }
     }
 
     private void GameInput_OnInteractAction(object sender, System.EventArgs e) {
+        if (!KitchenGameManager.Instance.IsGamePlaying()) return;
+
         if (selectedCounter != null) {
             selectedCounter.Interact(this);
         }
